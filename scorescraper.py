51600a33c35bfcf1f0dc8ad1e24efd8ce15fb650
airjesse123 <jesse.m.unger@gmail.com>
2015-11-26 20:58:05 +0000

    subreddits

--- scorescraper.py
+++ scorescraper.py
@@ -28,7 +28,8 @@ def getcbbthread(urlname,secret,token):
 	page = requests.get('http://sports.yahoo.com'+urlname)
 	page.content2 =  page.content.replace("\\", "")
 	tree=html.fromstring(page.content2) 
-	df=pd.read_csv('https://raw.githubusercontent.com/airjesse123/GameThreadGenerator/master/TeamLookup.csv', sep=',',header=None)
+	df1=pd.read_csv('https://raw.githubusercontent.com/airjesse123/GameThreadGenerator/master/TeamLookup.csv', sep=',',header=None)
+	df=df = df1.fillna('')
 
 	VisitingTeamName = tree.xpath('//div[@class="team away"]/div[@class="team-info"]/div[@class="name"]/a/text()')[0]
 	VisitingYahooName = tree.xpath('//div[@class="team away"]/div[@class="team-info"]/div[@class="name"]//@href')[0][13:-1]
